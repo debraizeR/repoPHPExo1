3c019c64aee64c0ced05160ac98ace9e721c9a91
debraizeR <76164950+debraizeR@users.noreply.github.com>
2021-01-22 09:37:09 +0100

    Add files via upload

---
+++ exo5/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+//settype($int, 'int');
+$int = null;
+var_dump($int);
+
+$int;
+var_dump($int);
+
+$int = 0; 
+var_dump($int);
+
+$int = 5;
+var_dump($int);
+?>
+</body>
+</html>
---
+++ exo8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$sum = 3+4;
+$difference= 100-20;
+$product = 45*15;
+$quotient = 45/15;
+$modulo = fmod(75,15);
+
+echo $sum." ".$difference." ".$product." ".$quotient." ".$modulo;
+
+?>
+</body>
+</html>
